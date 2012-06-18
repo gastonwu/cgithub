afe5c6f5827d5b7f09730de511ed4ed73c963254
GastonWu <gastonwu@gmail.com>
2012-06-18 11:49:27 +0800

    change bool to int

--- _release/bitmap/array.test.cpp
+++ _release/bitmap/array.test.cpp
@@ -9,7 +9,7 @@ int main(){
 	int million10 = 10 * million;
 	unsigned long billion = 1000 * million;
 	unsigned long large_number = 4 * billion;
-	bool * int_array[10*100000];
+	int * int_array[million];
 
 	printf("%u,%u,%u,%u\n",million,million10,billion,large_number);
 }
