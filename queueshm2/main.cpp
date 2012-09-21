32694011d74cdbf50aedb5f8d9f2a8da3d323600
GastonWu <gastonwu@gmail.com>
2012-09-21 15:17:12 +0800

    code backup

--- queueshm2/main.cpp
+++ queueshm2/main.cpp
@@ -13,16 +13,15 @@ struct Msg{
         char line[100];
 }typedef Msg;
 
-//typedef QueueShm<Msg, 10> NetDataQueueShm;
-//NetDataQueueShm* netdataQueue;
+typedef QueueShm<Msg, 10> NetDataQueueShm;
+NetDataQueueShm* netdataQueue;
 
 int main(int argc, char** argv) {
-//    QShm qshm;
-//    qshm.init(12345,sizeof(NetDataQueueShm));
-//    netdataQueue = (NetDataQueueShm *)qshm.getBuf();
     
-    QueueShm<Msg, 10> netdataQueue;
-//NetDataQueueShm* netdataQueue;
+    QShm qshm;
+    qshm.init(12345,sizeof(NetDataQueueShm));
+    netdataQueue = (NetDataQueueShm *)qshm.getBuf();
+    //QueueShm<Msg, 10> netdataQueue;
     
     Msg msg;
     memset(&msg,0,sizeof(msg));
@@ -30,19 +29,18 @@ int main(int argc, char** argv) {
     int loop=0;
     for(int i=0;i<10;i++){
         printf("[%d]\t",loop++);
-        netdataQueue.push(msg);
-        netdataQueue.printStatus();
+        netdataQueue->push(msg);
+        netdataQueue->printStatus();
     }
     printf("[%d]-\t",loop++);
-    netdataQueue.pop(msg);
-    netdataQueue.printStatus();
+    netdataQueue->pop(msg);
+    netdataQueue->printStatus();
     for(int i=0;i<5;i++){
-//        printf("(%c)\t",'-');netdataQueue.printStatus();
         printf("[%d]\t",loop++);
-        netdataQueue.push(msg);
-        netdataQueue.printStatus();
+        netdataQueue->push(msg);
+        netdataQueue->printStatus();
     }
     
-    netdataQueue.printStatus();
+    netdataQueue->printStatus();
     return 0;
 }
